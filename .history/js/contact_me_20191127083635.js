695e4b49c2ae39fa79dedf219efc2efb96316edc
DOODpls <floyd_santos64@yahoo.com>
2019-11-27 08:44:28 -0700

    trying email

--- .history/js/contact_me_20191127083635.js
+++ .history/js/contact_me_20191127083635.js
@@ -19,7 +19,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",                    //././mail/contact_me.php,  ../mail/contact_me.php
+                url: "https://formspree.io/mgevvkql",                    //././mail/contact_me.php,  ../mail/contact_me.php
                 type: "POST",
                 data: {
                     name: name,
