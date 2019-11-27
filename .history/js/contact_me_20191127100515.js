f2e9b8e3f7c058303c2f3761bd2e83868a0953d5
DOODpls <floyd_santos64@yahoo.com>
2019-11-27 12:09:06 -0700

    changed some of the projects to giv

---
+++ .history/js/contact_me_20191127100515.js
@@ -0,0 +1,20 @@
+// const aa = require('../mail/contact_me.php')
+$(function() {
+
+    $("input,textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        };
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
+});
